d3a44cedbf8d64bc4f8d021a01f186894124a6a6
rezeau <joseph@rezeau.org>
2022-06-29 18:34:10 +0200

    removed useless tests dir
    Signed-off-by: rezeau <joseph@rezeau.org>

--- tests/helpers/setup.js
+++
@@ -1,6 +0,0 @@
-require('browser-env')();
-const hooks = require('require-extension-hooks');
-
-// Setup vue files to be processed by `require-extension-hooks-vue`
-// Setup vue and js files to be processed by `require-extension-hooks-babel`
-hooks(['js']).plugin('babel').push();
